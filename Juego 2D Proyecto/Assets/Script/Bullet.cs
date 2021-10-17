866b0874d760ea3a4852a06ae3f6549d41d72b7a
ElCANC3R <diejoruiz1993@gmail.com>
2021-10-17 00:36:47 -0500

    Aqui les va esa

---
+++ Juego 2D Proyecto/Assets/Script/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D Rigidbody2D;
+    public float speed;
+    
+    void Start()
+    {
+        Rigidbody2D=GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        Rigidbody2D.velocity = Vector3.right*speed;
+    }
+}
